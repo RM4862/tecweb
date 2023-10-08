a69ccba3b02471d0368f675d0a811fe547efa549
RM <regina.avelinof@alumno.buap.mx>
2023-10-07 22:32:03 -0600

    p05 get_productos_xhtml.php completo cargado

--- practicas/p05/get_productos_xhtml.php
+++ practicas/p05/get_productos_xhtml.php
@@ -1,78 +1,81 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN” 
+“http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
-	<?php
-	if(isset($_GET['tope']))
-		$tope = $_GET['tope'];
 
-	if (!empty($tope))
-	{
-		/** SE CREA EL OBJETO DE CONEXION */
-		@$link = new mysqli('localhost', 'root', '12345678a', 'marketzone');	
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<?php
+    $data = array();
+    if(isset($_GET['tope'])) //verificamos de que haya un valor en la URL 
+        $tope = $_GET['tope']; //guardamos el tope dado en la URL en $tope
 
-		/** comprobar la conexión */
-		if ($link->connect_errno) 
-		{
-			die('Falló la conexión: '.$link->connect_error.'<br/>');
-			    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
-		}
+    if (!empty($tope))
+    {
+        /** SE CREA EL OBJETO DE CONEXION */
+        @$link = new mysqli('localhost', 'root', '12345678a', 'marketzone');	
 
-		/** Crear una tabla que nos devuelve un conjunto de resultados de la base de datos*/
-		if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") ) //localmente esta result
-		{   var_dump($result);
-			$row = $result->fetch_array(MYSQLI_ASSOC); ///arreglo row, fetch array es para sacar la consulta
-			/** útil para liberar memoria asociada a un resultado con demasiada información */
-			$result->free();
-		}
+        /** comprobar la conexión */
+        if ($link->connect_errno) 
+        {
+            die('Falló la conexión: '.$link->connect_error.'<br/>');
+                /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+        }
 
-		$link->close();
-	}
-	?>
-	<head> <!--empieza xhtml-->
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Producto</title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	</head>
-	<body>
-		<h3>PRODUCTO</h3>
+        /** Crear una tabla que no devuelve un conjunto de resultados si es que esta el id*/
+        if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") ) 
+        {
+            $row = $result->fetch_all(MYSQLI_ASSOC);
+            
+            $result->free();
+        }
 
-		<br/>
-		
-		<?php if( isset($row) ) : ?> <!--si el arreglo tiene info se muestra-->
+        $link->close();
+    }
+?>
 
-			<table class="table">
-				<thead class="thead-dark">
-					<tr>
-					<th scope="col">#</th>
-					<th scope="col">Nombre</th>
-					<th scope="col">Marca</th>
-					<th scope="col">Modelo</th>
-					<th scope="col">Precio</th>
-					<th scope="col">Unidades</th>
-					<th scope="col">Detalles</th>
-					<th scope="col">Imagen</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th scope="row"><?= $row['id'] ?></th>
-						<td><?= $row['nombre'] ?></td>
-						<td><?= $row['marca'] ?></td>
-						<td><?= $row['modelo'] ?></td>
-						<td><?= $row['precio'] ?></td>
-						<td><?= $row['unidades'] ?></td>
-						<td><?= utf8_encode($row['detalles']) ?></td>
-						<td><img src=<?= $row['imagen'] ?> ></td>
-					</tr>
-				</tbody>
-			</table>
+<body>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Marca</th>
+            <th scope="col">Modelo</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Unidades</th>
+            <th scope="col">Detalles</th>
+            <th scope="col">Imagen</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+			<?php 
+				if (isset($row) /*&& is_array($row)*/) {
+					foreach ($row as $registro) {
+						echo '<tr>';
+						echo '<td>' . $registro['id'] . '</td>';
+						echo '<td>' . $registro['nombre'] . '</td>';
+						echo '<td>' . $registro['marca'] . '</td>';
+						echo '<td>' . $registro['modelo'] . '</td>';
+						echo '<td>' . $registro['precio'] . '</td>';
+						echo '<td>' . $registro['unidades'] . '</td>';
+						echo '<td>' . $registro['detalles'] . '</td>';
+						echo '<td><img src="' . $registro['imagen'] . '"></td>';
+						echo '</tr>';
+					}
+				} else {
+					// si $row no tiene valores o no tiene información dentro del arreglo
+					echo '<tr><td colspan="8">No se encontraron registros.</td></tr>';
+				}
+			?> 
+            </tr>
+        </tbody>
+    </table>
 
-		<?php elseif(!empty($tope)) : ?>
-
-			 <script>
-                alert('No hay tope de unidades');
-             </script>
-
-		<?php endif; ?>
-	</body>
+</body>
+	
 </html>
