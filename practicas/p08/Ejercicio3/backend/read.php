b2587f66a17e482a11fe508e77e8f14288a76b20
RM <regina.avelinof@alumno.buap.mx>
2023-11-12 15:21:20 -0600

    Practica 8 ejercicio 3 read.php

--- practicas/p08/Ejercicio3/backend/read.php
+++ practicas/p08/Ejercicio3/backend/read.php
@@ -9,18 +9,6 @@
         // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
         if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$coincidencia}' OR nombre LIKE '%{$coincidencia}%' or 
         marca like '%{$coincidencia}%' or detalles like '%{$coincidencia}%'") ) {
-            // SE OBTIENEN solo un resultado
-			//$row = $result->fetch_array(MYSQLI_ASSOC);
-            
-            /*
-            if(!is_null($row)) {
-                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
-                foreach($row as $key => $value) {
-                    $data[$key] = utf8_encode($value);
-                }
-            }
-            */
-            
             /*en row se le pasan las filas, o registros, luego con el foreach va iterando de forma columna y valor y se le pasa al
             arreglo registro. data tendra en cada casilla un nuevo registro pasado por utf8 */
             while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
